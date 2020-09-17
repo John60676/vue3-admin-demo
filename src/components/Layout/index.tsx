981ea87dead64e2974c78d27a3e662b118de58e8
John <John60676@qq.com>
2020-09-17 14:15:38 +0800

    ✨feat(layout): add layout fixed

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref, KeepAlive } from 'vue';
+import { defineComponent, ref, KeepAlive, computed } from 'vue';
 import { Layout } from 'ant-design-vue';
 import Menu from '@/components/Menu';
 import Logo from '@/components/Logo';
@@ -14,30 +14,48 @@ export default defineComponent({
   name: namespace,
   setup() {
     const isCollapsed = ref(false);
-    return () => (
-      <Layout class={bem()}>
-        <Layout.Sider class={bem('sider')} collapsible v-model={[isCollapsed.value, 'collapsed']}>
-          <Logo />
-          <Menu />
-        </Layout.Sider>
-        <Layout>
-          <Layout.Header class={bem('header')}>
-            <NavBar />
-            <TagBar />
-          </Layout.Header>
-          <Layout.Content class={bem('content')}>
-            <router-view
-              v-slots={{
-                default: ({ Component }) => (
-                  <KeepAlive>
-                    <Component />
-                  </KeepAlive>
-                ),
-              }}
-            />
-          </Layout.Content>
+    const siderWidth = 200; // 菜单栏展开宽度
+    const collapsedWidth = 80; // 菜单栏折叠宽度
+    const siderWidthRef = computed(() => (isCollapsed.value ? collapsedWidth : siderWidth));
+
+    return () => {
+      const headerStyle = {
+        width: `calc(100% - ${siderWidthRef.value}px)`,
+      };
+      const layoutRightStyle = {
+        marginLeft: `${siderWidthRef.value}px`,
+      };
+      return (
+        <Layout class={bem()}>
+          <Layout.Sider
+            class={[bem('sider'), bem('left-wrap')]}
+            v-model={[isCollapsed.value, 'collapsed']}
+            collapsedWidth={collapsedWidth}
+            width={siderWidth}
+            collapsible
+          >
+            <Logo />
+            <Menu />
+          </Layout.Sider>
+          <Layout class={bem('right-wrap')} style={layoutRightStyle}>
+            <Layout.Header class={bem('header')} style={headerStyle}>
+              <NavBar />
+              <TagBar />
+            </Layout.Header>
+            <Layout.Content class={bem('content')}>
+              <router-view
+                v-slots={{
+                  default: ({ Component }) => (
+                    <KeepAlive>
+                      <Component />
+                    </KeepAlive>
+                  ),
+                }}
+              />
+            </Layout.Content>
+          </Layout>
         </Layout>
-      </Layout>
-    );
+      );
+    };
   },
 });
