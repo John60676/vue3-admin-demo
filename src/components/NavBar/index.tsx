9c379a1b1ee22a2cc6ea62e66a3a7fdd62b8b273
John <John60676@qq.com>
2020-09-22 16:55:37 +0800

    ✨feat(navBar): add logout event

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,6 +5,8 @@ import NavBarAvatarMenu from '@/components/NavBarAvatarMenu';
 import createNamespace from '@/utils/bem';
 
 import './index.scss';
+import { useRouter } from 'vue-router';
+import { ROUTE_LOGIN } from '@/router/constantRoutes';
 
 const namespace = 'sx-navbar';
 const bem = createNamespace(namespace);
@@ -12,11 +14,17 @@ const bem = createNamespace(namespace);
 export default defineComponent({
   name: namespace,
   setup() {
+    const router = useRouter();
+    const handleLogout = () => {
+      window.localStorage.setItem(ROUTE_LOGIN, '0');
+      router.replace({ name: ROUTE_LOGIN });
+    };
+
     return () => (
       <div class={bem()}>
         <Breadcrumb />
         <div class={bem('nav-tools')}>
-          <NavBarAvatarMenu />
+          <NavBarAvatarMenu onLogout={handleLogout} />
         </div>
       </div>
     );
--- src/components/NavBarAvatarMenu/index.tsx
+++ src/components/NavBarAvatarMenu/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import { Dropdown, Menu, Avatar } from 'ant-design-vue';
 import classNames from 'classnames';
 import createNamespace from '@/utils/bem';
@@ -10,7 +10,12 @@ const bem = createNamespace(namespace);
 
 export default defineComponent({
   name: namespace,
-  setup() {
+  props: {
+    onLogout: {
+      type: Function as PropType<() => void>,
+    },
+  },
+  setup(props) {
     const menuSlot = () => (
       <Menu class={bem('menu')}>
         <Menu.Item key='1' title='个人中心'>
@@ -19,7 +24,7 @@ export default defineComponent({
         <Menu.Item key='2' title='修改密码'>
           修改密码
         </Menu.Item>
-        <Menu.Item key='3' title='退出登录'>
+        <Menu.Item key='3' title='退出登录' onClick={props.onLogout}>
           退出登录
         </Menu.Item>
       </Menu>
