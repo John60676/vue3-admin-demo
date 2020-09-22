b87b0f7532e6710d440c00a3ebdd7ed97089b93d
John <John60676@qq.com>
2020-09-22 10:31:14 +0800

    🦄refactor(breadcrumb): add redirect type judgment

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -49,7 +49,7 @@ export default defineComponent({
     });
 
     const handleRoutePush = (routeItem: MatchRouteType) => {
-      if (routeItem.redirect) {
+      if (routeItem.redirect && typeof routeItem.redirect !== 'function') {
         router.push(routeItem.redirect);
       } else {
         router.push(routeItem.path);
